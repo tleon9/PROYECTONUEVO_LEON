082ee08874a00d7229f015cd4b9d73c7b7a9ad67
tleon <tanialeonp@gmail.com>
2023-02-11 13:03:08 -0800

    version 3

--- proyecto.js
+++ proyecto.js
@@ -6,54 +6,102 @@ class simulador{
         this.plazo = plazo,
         this.mensualidad= mensualidad
     }
-    mostrartexto(){
-        console.alert(`${this.nombre}, tu edad es ${this.edad}, el monto que deseas solicitar es por valor de ${this.monto} 
-        y el plazo es de ${this.plazo} meses. Pagarias un valor 
-        de ${this.mensualidad} mensuales.Tenga en cuenta que este cálculo se 
-        realizó con una tasa de interes de 2.33% efectiva mensual y que en esta cuota no se incluyen seguros`)
+        mostrartexto(){
+            console.alert(`${this.nombre}, tu edad es ${this.edad}, el monto que deseas solicitar es por valor de ${this.monto} 
+            y el plazo es de ${this.plazo} meses. Pagarias un valor 
+            de ${this.mensualidad} mensuales.Tenga en cuenta que este cálculo se 
+            realizó con una tasa de interes de 2.33% efectiva mensual y que en esta cuota no se incluyen seguros`)
+        }
     }
-}
 
-const prestamos = []
+let prestamos = []
+
 
 function agregarSimulacion(array){
-    let nombreIngresado = prompt("Bienvenido/a al simulador de creditos, por favor ingrese su nombre")
-    let edad = parseInt(prompt("Ingrese su edad actual"))
-        if (edad >= 18){
-        console.log("Edad válida para solicitar un prestamo")
-        }
-        else{
-        console.log("Usted no puede solicitar un prestamo, edad no válida")
-        }
-    let monto = parseInt(prompt(`Ingrese el valor del prestamo que desea solicitar,recuerde que el valor mínimo es de 100 usd y el valor máximo son 1000000 usd`))
-        if ( monto >= 100 && monto < 1000000){
-            console.log(`El valor ingresado : ${monto} es válido`)
-        }
-        else{
-            sistema = false
-            console.log(`El monto ingresado ${monto} no es válido, recuerde que el valor 
-            mínimo es de 100 usd y el valor máximo son 1000000 usd`) 
-        }
-    let plazo = parseInt(prompt(`Ingrese el plazo al cual desea solicitar su credito,tenga en cuenta que el plazo minimo es de 12 meses y el máximo es de 84 meses`))
-        if (plazo <= 84  && plazo >= 12 ){
-            console.log(`El plazo ingresado : ${plazo} es válido`)        
-        }
-        else{
-            console.log(`El plazo ingresado ${plazo} no es válido, recuerde que el plazo minimo es de 12 meses y el máximo es de 84 meses`)
-            sistema = false
-        }
-    
-    let numerador = Math.pow((1 + 0.0233),plazo) *(0.0233)
-    let denominador = Math.pow((1 + 0.0233), plazo) - 1
-    let mensualidad = monto*(numerador/denominador)
-    console.log(`Por un crédito de ${monto} a un plazo de ${plazo} meses, pagarias un valor de ${mensualidad} mensuales.Tenga en cuenta que este calculo se realiza con una tasa de interes de 2.33% efectiva mensual y que en esta cuota no se incluyen seguros`)    
-    const nuevosPrestamo = new simulador(nombreIngresado, edad, monto,plazo,mensualidad)
+    let nombre = document.getElementById("nombreIngresado").value;
+    let edad = parseInt(document.getElementById("edadIngresada").value);
+    let monto = parseInt(document.getElementById("montoIngresado").value);
+    let plazo = parseInt(document.getElementById("plazoIngresado").value);
+    validarFormulario(array)
+    let mensualidad = calculoCuota(monto,plazo);
 
-    console.log(nuevosPrestamo)
+    const nuevosPrestamo = new simulador(nombre, edad, monto,plazo, mensualidad);
+
+
+    localStorage.setItem("prestamos", JSON.stringify(array))
+    verSimulaciones(array)
     
     array.push(nuevosPrestamo)
-    console.log(array)
+
+    let agregarSmlcin = document.getElementById("agregarSmlcin");
+
+
+    agregarSmlcin.reset();
 
 }
-agregarSimulacion(prestamos)
 
+let calcular = document.getElementById("calcularbtn")
+
+
+calcular.onclick = function(){
+    agregarSimulacion(prestamos)
+
+}
+
+
+
+function calculoCuota(m,p){
+    
+    let numerador = Math.pow((1 + 0.0233),p) *(0.0233)
+    let denominador = Math.pow((1 + 0.0233), p) - 1
+    let mensualidad = m*(numerador/denominador)
+    return mensualidad;
+}
+
+
+
+
+let pagosDiv = document.getElementById("pagomensual")
+
+function verSimulaciones(array){
+    pagosDiv.innerHTML = "" 
+
+    for(let pago of array){
+        
+        
+        let nuevoCalculo = document.createElement("div")
+        nuevoCalculo.className = "col-9"
+        nuevoCalculo.innerHTML = ` <div class="alert alert-primary" role="alert">
+        Por un crédito de ${pago.monto} a un plazo de ${pago.plazo} meses, pagarias un valor de ${pago.mensualidad} mensuales.Tenga en cuenta que este calculo se realiza con una tasa de interes de 2.33% efectiva mensual y que en esta cuota no se incluyen seguros.
+        </div>` 
+        pagosDiv.appendChild(nuevoCalculo)
+    }
+
+}
+
+
+function validarFormulario(){
+    let edad = parseInt(document.getElementById("edadIngresada").value);
+    let mensajeError = document.getElementById("mensajeError");
+    if (edad < 18){
+        mensajeError.innerHTML = "Usted no puede solicitar un prestamo, edad no válida";
+        return;
+    }
+    let monto = parseInt(document.getElementById("montoIngresado").value);
+    if(monto <  100 && monto > 1000000){
+        mensajeError.innerHTML = `El monto ingresado ${monto} no es válido, recuerde que el valor mínimo es de 100 usd y el valor máximo son 1000000 usd`;
+        return;
+    }
+    let plazo = parseInt(document.getElementById("plazoIngresado").value);
+    if (plazo  >= 84  && plazo < 12 ){
+        mensajeError.innerHTML = `El plazo ingresado ${plazo} no es válido, recuerde que el plazo minimo es de 12 meses y el máximo es de 84 meses`;
+        return;
+    }
+    if (edad === "" || monto === "" || plazo === "") {
+        mensajeError.innerHTML = "Todos los campos son obligatorios";
+        return;
+      }
+}
+
+
+  
